91cc92a1c99b2dcf8b28b9e524a524352afcc2c5
david <davidmatovu88@gmail.com>
2022-08-08 11:36:54 +0300

    Add  requests: Get videos by tag and by title

--- server/routes/videos.js
+++ server/routes/videos.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import {verifyToken} from "../verifyToken.js"
-import{ addVideo, addView, deleteVideo, getVideo, random, sub, trend, updateVideo } from "../controllers/video.js"
+import{ addVideo, addView, deleteVideo, getVideo, random, search, sub, trend, updateVideo, getByTag} from "../controllers/video.js"
 
 
 const router = express.Router()
@@ -27,11 +27,13 @@ router.get("/find/:id", verifyToken, getVideo)
 
 router.put("/views/:id", verifyToken, addView)
 
-router.get("/trend", verifyToken, trend)
+router.get("/trend", trend)
 
-router.get("/rendom", verifyToken, random)
+router.get("/random", random)
 
 router.get("/sub", verifyToken, sub)
+router.get("/tags", getByTag)
+router.get("/search", search)
 
 
 
