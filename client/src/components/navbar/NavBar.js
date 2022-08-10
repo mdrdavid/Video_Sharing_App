8aa5726899f8982328bb552bc14b1d10a8ebab6c
david <davidmatovu88@gmail.com>
2022-08-10 14:32:42 +0300

    change div name to nav-container

--- client/src/components/navbar/NavBar.js
+++ client/src/components/navbar/NavBar.js
@@ -7,7 +7,7 @@ import {Link} from "react-router-dom"
 
 const NavBar =()=>{
     return(
-        <div className="container">
+        <div className="nav-container">
              <Link to="/" style={{ textDecoration: "none", color: "inherit" }}>
             <Logo/>
             </Link>
