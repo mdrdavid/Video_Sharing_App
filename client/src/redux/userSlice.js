8d222046474f096688d0c9c32feb987288ca9a7e
david <davidmatovu88@gmail.com>
2022-08-11 22:57:53 +0300

    create redux store

--- client/src/redux/userSlice.js
+++ client/src/redux/userSlice.js
@@ -2,7 +2,32 @@
 import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = {
-  user: null,
+  currentUser: null,
   loading:false,
  error: false
 }
+
+export const userSlice = createSlice({
+    name: 'user',
+    initialState,
+    reducers: {
+      loginStart: (state)=>{
+          state.loading=true
+      },
+      loginSuccess: (state, action)=>{
+        state.loading=false
+        state.user = action.payload
+    },
+    loginFailure: (state)=>{
+        state.loading=false
+        state.error=true
+    },
+    logout:(state)=>{
+        state= initialState
+    }
+    },
+  })
+
+  export const{loginStart, loginSuccess, loginFailure, logout}= userSlice.actions
+
+  export default userSlice.reducer
