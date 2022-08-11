55b80eb4ba427bb4a4e2ff7e067189dd51e0b1a7
david <davidmatovu88@gmail.com>
2022-08-11 22:58:34 +0300

    add redux to nav bar

--- client/src/components/navbar/Button.js
+++ client/src/components/navbar/Button.js
@@ -2,17 +2,31 @@ import React from 'react'
 import "./navbar.css"
 import {Link} from "react-router-dom"
 import {MdOutlineAccountCircle} from "react-icons/md"
+import { useSelector } from "react-redux"
+import {MdMissedVideoCall} from "react-icons/md"
+import {FaUserAlt} from "react-icons/fa"
 
 const Button = () => {
+    const {currentUser} = useSelector(state=>state.user)
     return (
         <>
+        {currentUser ? (
+            <div className="user">
+                <MdMissedVideoCall/>
+            <div className ="avator">
+                <FaUserAlt/>
+                </div>
+            </div>
+        ) :
+        (
             <Link to="/signin" className="link">
-                <button className='button'>
-                    <MdOutlineAccountCircle />
-                    SIGN IN
-                </button>
-            </Link>
-
+            <button className='button'>
+                <MdOutlineAccountCircle />
+                SIGN IN
+            </button>
+        </Link>
+        )
+    }
         </>
     )
 }
