85576c88ff46b680d5ff0f85abf28ac8c4ea9329
david <davidmatovu88@gmail.com>
2022-08-14 12:49:09 +0300

    create comments component

---
+++ client/src/components/comments/Comments.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import "./comments.css"
+
+const  Comments =() => {
+  return (
+    <div className='container'>
+      <img src="https://yt3.ggpht.com/yti/APfAmoE-Q0ZLJ4vk3vqmV4Kwp0sbrjxLyB8Q4ZgNsiRH=s88-c-k-c0x00ffffff-no-rj-mo" />
+      <div className='details'>
+        <p className='name'>
+          John Doe <span className='date'>1 day ago</span>
+        </p>
+        <p className='text'>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel, ex
+          laboriosam ipsam aliquam voluptatem perferendis provident modi, sequi
+          tempore reiciendis quod, optio ullam cumque? Quidem numquam sint
+          mollitia totam reiciendis?
+        </p>
+      </div>
+    </div>
+  )
+}
+export default Comments
